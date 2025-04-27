b35403fe5ec008ed7a855a345696d131a45a5d20
J3FFR3Y-E-K <kouevij1@montclair.edu>
2025-04-26 22:18:11 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -26,11 +26,33 @@ else
 	die();
 }*/
 
+/*
 if($rs) {
     echo "Signup successful! <a href='//cyan.csam.montclair.edu/~lovei1/login.html'>Now please login here!</a>";
 } else {
     echo "Signup failed, <a href='//cyan.csam.montclair.edu/~lovei1/signup.html'>please try again.</a>";
 }
+*/
+if($rs) {
+    header("Location: //cyan.csam.montclair.edu/~lovei1/login.html");
+    exit(); // Always use exit() after header to ensure the script stops.
+} else {
+    header("Location: //cyan.csam.montclair.edu/~lovei1/signup.html");
+    exit();
+}
 
+/*
+if($rs) {
+    echo "Signup successful! You will be redirected shortly. If not, <a href='//cyan.csam.montclair.edu/~lovei1/login.html'>click here to login</a>.";
+    echo "<script>setTimeout(function() {
+        window.location.href = '//cyan.csam.montclair.edu/~lovei1/login.html';
+    }, 5000);</script>";
+} else {
+    echo "Signup failed! You will be redirected shortly. If not, <a href='//cyan.csam.montclair.edu/~lovei1/signup.html'>click here to try again</a>.";
+    echo "<script>setTimeout(function() {
+        window.location.href = '//cyan.csam.montclair.edu/~lovei1/signup.html';
+    }, 5000);</script>";
+}
+*/
 
 ?>
