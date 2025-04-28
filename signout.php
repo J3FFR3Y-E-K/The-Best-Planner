b7706c02e4a289842b0c839c5d7fae6a3d042939
Ian Love <131482976+IanKLove@users.noreply.github.com>
2025-04-28 16:46:30 -0400

    Update signout.php
    Added signout for all login types

--- signout.php
+++ signout.php
@@ -1,5 +1,7 @@
 <?php
 setcookie("Login", "", time() - 3600, "/");
+setcookie("Organization", "", time() - 3000, "/");
+setcookie("Admin", "", time() - 3000, "/");
 header("Location: //cyan.csam.montclair.edu/~lovei1/login.html");
 die();
 ?>
