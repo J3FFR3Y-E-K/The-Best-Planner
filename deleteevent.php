de46e493673884576f626684850493c8ee99025f
Ian Love <131482976+IanKLove@users.noreply.github.com>
2025-04-29 14:25:37 -0400

    Update deleteevent.php
    Fixed small bugs

--- deleteevent.php
+++ deleteevent.php
@@ -10,21 +10,28 @@ if ($con->connect_error) {
 }
 
 $id = $_POST['eventID'];
-
+/*?><script>alert(<?php echo $id; ?>);</script><?php*/
 $sql = "DELETE FROM `events` WHERE `EID` = '$id'";
 
 $rs = mysqli_query($con, $sql);
 
-if($rs)
+$sql2 = "DELETE FROM `rsvps` WHERE `EID` = '$id'";
+$rs2 = mysqli_query($con, $sql2);
+
+if($rs && $rs2)
 {
     $message = "Event Deleted Succesfully";
     header("Location: https://cyan.csam.montclair.edu/~lovei1/orgeventlist.php?message=".$message);
 	die();
 }
-else
+elseif($rs && !$rs2)
 {
-	session_start();
-    $message = "Event Could Not Be Deleted";
+    $message = "Event Deleted, but RSVP's not deleted";
+    header("Location: https://cyan.csam.montclair.edu/~lovei1/orgeventlist.php?message=".$message);
+	die();
+} else
+{
+    $message = "Event Couldn't Be Deleted";
     header("Location: https://cyan.csam.montclair.edu/~lovei1/orgeventlist.php?message=".$message);
 	die();
 }
